37a8006b3fe25aa61b5fd3eabd4c9c36a388d60e
Michael Boroski <mjboroski@gmail.com>
2018-04-22 06:22:04 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+
+  @@count=0
+  @@artists=[]
+  @@genres=[]
+
+  attr_accessor :name, :artist,:genre
+
+  initialize()
+    @@count+=1
+  end
+
+  def count
+    @@count
+  end
+
+  def artist=(artist)
+     if @@artists.detect {|i| i==artist} == nil
+      @@artists<<artist 
+    else @@artists[artist]+=1
+    end
+end
