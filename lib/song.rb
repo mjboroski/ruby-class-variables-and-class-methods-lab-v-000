80025257ff4107feba49a06d4339cbe80d225b48
Michael Boroski <mjboroski@gmail.com>
2018-04-22 06:56:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,11 +26,7 @@ class Song
   def self.artist_count
     artist_count={}
     @@artists.each do |artist|
-      if artist_count.artist
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
+      artist_count[artist] ? artist_count[artist] += 1 : artist_count[artist] = 1
     end
     artist_count
   end
@@ -42,11 +38,7 @@ class Song
   def self.genre_count
     genre_count={}
     @@genres.each do |genre|
-      if genre_count.genre
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
+      genre_count[genre] ? genre_count[genre] += 1 : genre_count[genre] = 1
     end
     genre_count
   end
